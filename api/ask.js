837be7df2eb31b4500005db3720770b21246b2ab
BingBong12367 <Nebi6304@gmail.com>
2026-02-27 21:15:20 +0100

    Update ask.js

--- api/ask.js
+++ api/ask.js
@@ -1,10 +1,10 @@
-export default async function handler(req, res) {
+export default async function handler(req,res){
 
 if(req.method !== "POST"){
 return res.status(400).json({error:"POST only"});
 }
 
-const {prompt} = req.body;
+const {prompt,image} = req.body;
 
 const response = await fetch(
 "https://api.openai.com/v1/chat/completions",
@@ -14,19 +14,42 @@ headers:{
 "Content-Type":"application/json",
 "Authorization":`Bearer ${process.env.OPENAI_API_KEY}`
 },
+
 body:JSON.stringify({
+
 model:"gpt-4o-mini",
+
 messages:[
 {
 role:"system",
-content:"Du bist ein wissenschaftlicher Hundeexperte."
+content:
+"Du bist ein wissenschaftlicher Hunde-Verhaltensforscher. Analysiere Emotionen, Stresssignale, Körpersprache und Rasseunterschiede."
 },
+
 {
 role:"user",
-content:prompt
+
+content:[
+{
+type:"text",
+text: prompt
+},
+
+image ? {
+type:"image_url",
+image_url:{
+url:image
 }
+} : null
+
+].filter(Boolean)
+
+}
+
 ]
+
 })
+
 });
 
 const data = await response.json();
